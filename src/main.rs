498a7fcb6ff5fc4fa360b432e3909210a7e49928
seebazz <bretsebastian@protonmail.com>
2022-04-25 18:27:12 -0500

    still working on issues

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    let measure_question = "Choose either Fahrenheit (f) or Celsius (c)";
+
+    let value_input: i32;
+    
+    let measure_input = get_input(measure_question);
+    println!("{}", measure_input.eq("f"));
+    if measure_input.eq("f") {
+        println!("You want to convert Fahrenheit to Celsius.");
+        let value_question = "What is the value in Fahrenheit?";
+        value_input = get_input(value_question).trim().parse().unwrap();
+        println!("{} degrees fahrenheit converts to {} degrees celsius.", value_input, value_input - 32)
+
+    } else if measure_input.eq("c") {
+        println!("You want to convert Celsius to Fahrenheit.");
+        let value_question = "What is the value in Celsius?";
+        value_input = get_input(value_question).trim().parse().unwrap();
+        println!("{} degrees fahrenheit converts to {} degrees celsius.", value_input, value_input + 32)
+    } else {
+        println!("Please either choose (c) or (f)")
+    }
+}
+
+fn get_input(question:&str) -> String {
+    let mut buffer = String::new();
+    println!("{}", question);
+    std::io::stdin().read_line(&mut buffer).expect("Failed");
+    buffer
 }
