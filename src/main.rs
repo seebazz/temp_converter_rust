e3f034c442e3f8aa6bc0042aa9259b156970bbc2
seebazz <bretsebastian@protonmail.com>
2022-04-25 19:44:17 -0500

    added a function to strip trailing newline

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,23 @@ fn main() {
     let value_input: i32;
     
     let measure_input = get_input(measure_question);
-    println!("{}", measure_input.eq("f"));
 
-    match measure_input.as_str() {
-        "f" => {
+    match strip_trailing_newline(measure_input.as_str()) {
+        "f" | "F" | "fahrenheit" | "Fahrenheit" => {
             println!("You want to convert Fahrenheit to Celsius.");
             let value_question = "What is the value in Fahrenheit?";
             value_input = get_input(value_question).trim().parse().unwrap();
             println!("{} degrees fahrenheit converts to {} degrees celsius.", value_input, value_input - 32)
         },
-        "c" => {
+        "c" | "celsius" | "Celsius" => {
             println!("You want to convert Celsius to Fahrenheit.");
             let value_question = "What is the value in Celsius?";
             value_input = get_input(value_question).trim().parse().unwrap();
-            println!("{} degrees fahrenheit converts to {} degrees celsius.", value_input, value_input + 32)
+            println!("{} degrees celsius converts to {} degrees fahrenheit.", value_input, value_input + 32)
         },
         _ => {
-            println!("Please either choose (c) or (f)")
+            println!("{}", measure_input.as_str());
+            println!("Please either choose (c) or (f)");
         }
     }
 }
@@ -30,4 +30,11 @@ fn get_input(question:&str) -> String {
     println!("{}", question);
     std::io::stdin().read_line(&mut buffer).expect("Failed");
     buffer
+}
+
+fn strip_trailing_newline(input: &str) -> &str {
+    input
+        .strip_suffix("\r\n")
+        .or(input.strip_suffix("\n"))
+        .unwrap_or(input)
 }
